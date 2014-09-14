a9d42901a695c71dd3f9e453774dba5ecc08b0a6
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-09-14 10:08:08 -0700

    initial project.mk added

---
+++ project.mk
@@ -0,0 +1,15 @@
+PROJECT=jdksdap
+PROJECT_NAME=jdksdap
+PROJECT_VERSION=20140914
+PROJECT_EMAIL=jeffk@jdkoftinoff.com
+PROJECT_LICENSE=BSD
+PROJECT_MAINTAINER=jeffk@jdkoftinoff.com
+PROJECT_COPYRIGHT=Copyright 2014
+PROJECT_DESCRIPTION=jdksdap
+PROJECT_WEBSITE=http://github.com/jdkoftinoff/jdksdap
+PROJECT_IDENTIFIER=com.jdkoftinoff.jdksdap
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
+
+
