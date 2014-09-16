a8238456c7b19d2aba0114a2f5b3bfe03d8f9349
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-09-16 13:33:58 -0700

    Add C++11 to common.cmake

--- common.cmake
+++ common.cmake
@@ -12,6 +12,7 @@ if(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake|MSBuild)")
     add_definitions("/W2")
 else()
     add_definitions("-Wall -Wextra")
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++11 -stdlib=libc++")    
 endif()
 
 if(TODO MATCHES "ON")
